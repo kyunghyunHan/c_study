362ddcb59c6eb7e2c3e711ec46684b405a6c4807
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-01-19 20:53:45 +0900

    ㅁㅇㅇ

--- ch2/2_2_4.c
+++ ch2/2_2_4.c
@@ -4,5 +4,11 @@ int main(void)
 {
     float f = 46.5f;
     printf("%f\n", f);
+    //둘쨰짜리 까만 출력
+    printf("%.2f\n", f);
+    double d= 4.428;
+    //둘째자리 까지만 출력
+    printf("%.2lf\n", d);
     return 0;
+    /*double float실수인데 float에만 f를붙이는 이유= flaot이라는 것을 알리기 위해*/
 }
---
+++ ch2/2_3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(void){
+    //값을 변경할수 없음
+   const int YEAR= 1972;
+    printf("c언어가 발표된 연도 : %d\n",YEAR);
+    return 0;
+}
