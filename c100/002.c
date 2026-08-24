3a0274388a1567bde5c13eb183ed8ec346431657
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-08-24 12:41:07 +0900

    feat:u[date

---
+++ c100/002.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int a[10];
+    int dice[30];
+    int rand_min = 1;
+    int rand_max = 6;
+
+    srand(time(0)); //현재시간으로 랜덤시드
+
+    for(int i = 0;i<10;i++){
+        a[i] = rand();
+    }
+    printf("RAND_MAX = %x, %d\n",RAND_MAX,RAND_MAX);
+
+    for(int i =0;i<10;i++){
+        printf("%6d",a[i]);
+    }
+    printf("\n");
+    for(int i  =0;i<30;i++){
+        dice[i] = (double)rand() / RAND_MAX * (rand_max - rand_min+1)+rand_min;
+    }
+
+    for(int i = 0;i<30;i++){
+        printf("%6d%c",dice[i],(i+1)%10 !=0?' ':'\n');
+    }
+
+}
--- cp/day1.c
+++ cp/day1.c
@@ -1,14 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 #if 0 
 int main(void){
     printf("he");
     return 0;
 }
 #endif 
-
+#if 0
 int main(void){
     printf("Be happy!");
-    system("ls");
+    system("ls");//시스템 명령어
     return 0;
 }
+#endif 
+#if  0
+int main(void){
+    printf ("Be happy\n");
+    printf("1234\n");
+    printf("My\tfriend\n");
+    printf("Cow\rW\a\n");
+    return 0;
+}
+#endif
+#if 0
+int main(void){
+    printf("%.1lf\n",3.45);
+    return 0;
+}
+#endif
+#if 0
+int main(void){
+    printf("%d\n",1010);
+}
+#endif
+#if 0
+int main(){
+    short sh = 32767;
+    int in  = 2147483674;
+    return 0;
+}
+#endif
+#if 1
+int main(){
+    int a[10];
+    int min, max;
+    int sum = 0;
+
+    for (int i = 0; i<10;i++){
+        printf("%d번쨰 숫자를 입력하세요:",i+1);
+        scanf("%d",&a[i]);
+    }
+    for (int i = 0;i<10;i++){
+        printf("%6d",a[i]);
+    }
+    printf("\n");
+    min = max = a[0];
+
+    for(int i = 1;i<10;i++){
+        sum+=a[i];
+        if(a[i]<min)
+            min = a[i];
+        else if (a[i]>max){
+            max = a[i];
+        }
+    }
+    printf("min = %d,max = %d,average = %.2f\n",min,max,sum/10.0);
+}
+
+#endif
+//2진수 풀력
+#if 0 
+int main(void)
+{
+     int n = 100;
+
+    for (int i = 31; i >= 0; i--) {
+        printf("%u", (n >> i) & 1);
+    }
+
+    printf("\n");
+    return 0;
+}
+#endif
