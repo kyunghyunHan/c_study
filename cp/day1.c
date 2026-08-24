4cc8a3c33361ddc860358922d33e95d079e74235
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-08-24 11:22:28 +0900

    update

---
+++ cp/day1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#if 0 
+int main(void){
+    printf("he");
+    return 0;
+}
+#endif 
+
+int main(void){
+    printf("Be happy!");
+    system("ls");
+    return 0;
+}
