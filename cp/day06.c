2602e7f5f870fcc0b3659849a86644c2262c3bf9
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-08-31 10:12:05 +0900

    update

--- cp/day06.c
+++ cp/day06.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include "day06_lib.h"
+void swap(int *pa, int *pb){
 
+    int temp; 
+    temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+
+}
 int main(void) {
-    printf("%d\n", m);
+    int a = 10;
+    int b = 20;
+    swap(&a,&b);
+    // printf("%d\n", m);
+
+    printf("%d %d",a,b);
     return 0;
 }
