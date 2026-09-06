916ed18fd1aff410b19ea5c1f5d14881c4323c7b
kyunghyunHan <ekfqlwcjswl@naver.com>
2026-09-06 22:05:43 +0900

    update

--- project/simple_sort.c
+++ project/simple_sort.c
@@ -4,7 +4,16 @@
 /*
 정렬
 */
-
+void print_arr(int (*arr)[3], size_t rows)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            printf("%d%s", arr[i][j], j == 2 ? "\n" : " ");
+        }
+    }
+}
 int com(const void *a, const void *b)
 {
     int ca = *(int *)a;
@@ -29,8 +38,9 @@ void swap(void *a, void *b, int size)
     memmove(b, temp, size);
     free(temp);
 }
-void ssort(int *arr, size_t len, size_t size, int (*comp)(const void *, const void *))
+void ssort(void *base, size_t len, size_t size, int (*comp)(const void *, const void *))
 {
+    char *arr = base;
     // 마지막 -1까지
     for (int i = 0; i < len - 1; i++)
     {
@@ -40,14 +50,25 @@ void ssort(int *arr, size_t len, size_t size, int (*comp)(const void *, const vo
             // 비교
             // 첫번쨰 배열부터 해서 값을 비교
             // 0보다 크면 스왑
-            //오름차순
-            //내림차순 하고싶다면 반대
-            if (comp(arr + i, arr + j) > 0)
+            // 오름차순
+            // 내림차순 하고싶다면 반대
+            void *a = arr + i * size;
+            void *b = arr + j * size;
+            if (comp(a, b) > 0)
             {
-                swap(arr + i, arr + j, size);
+                swap(a, b, size);
             }
         }
     }
+}
+void sssort(int (*arr)[3], size_t rows)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        ssort(arr[i], 3, sizeof(arr[i][0]), com);
+    }
+
+    ssort(arr, rows, sizeof(arr[0]), com);
 }
 int main(void)
 {
@@ -60,11 +81,9 @@ int main(void)
         {5, 3, 4},
     };
     // c의 qsort
-    ssort(arr, 5, sizeof(arr + 0), com);
-
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d\n", arr[i][0]);
-    }
+    // ssort(arr, 5, sizeof(arr[0]), com);
+    //++
+    sssort(arr, 5);
+    print_arr(arr, 5);
     return 0;
 }
